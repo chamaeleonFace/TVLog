9f306fad2a679b8123a31caf987632da97b93d02
chamaeleonFace <birdman.wonder.world@gmail.com>
2024-05-08 07:47:16 +0900

    TVLogのdocker PHPファイルの新規作成

---
+++ src/TVLog.php
@@ -0,0 +1,14 @@
+<?php
+
+//視聴時間の登録
+echo 'テレビの視聴ログを登録してください' . PHP_EOL;
+echo 'テレビチャンネル :';
+echo fgets(STDIN);
+
+
+//視聴時間を表示
+echo 'テレビチャンネル' . PHP_EOL;
+echo '視聴分数' . '視聴回数' . PHP_EOL;
+
+echo 'テレビチャンネル' . PHP_EOL;
+echo '視聴分数' . '視聴回数' . PHP_EOL;
